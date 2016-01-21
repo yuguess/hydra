519be045777de3119a0d34fdef11df2ccc0a8766
fwang <wangfzy@gmail.com>
2016-01-21 15:25:49 +0800

    add symlink taht always point to the latest log file
    Former-commit-id: b79c3cfb6a8a30d3bb1c4db1419c49f19ef47da2 [formerly d75fa994cda25b8858935a417062de6d4428aa62]
    Former-commit-id: 2cbaa74225ca26641aef86eaf9bf8f3bf0ccc623

--- CedarHelper/include/CedarLogging.h
+++ CedarHelper/include/CedarLogging.h
@@ -2,6 +2,9 @@
 #define CEDAR_LOGGING_H
 
 #include "easylogging++.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 
 INITIALIZE_EASYLOGGINGPP
 
@@ -26,6 +29,42 @@ public:
         el::ConfigurationType::Filename, destDir + program + "_FATAL_%datetime{%Y%M%d-%H%m%s.%g}");
 
     el::Loggers::reconfigureLogger("default", defaultConf);
+
+    el::Logger* l = el::Loggers::getLogger("default");
+    int fd = open(destDir.c_str(), O_RDONLY);
+
+
+    std::string filename = l->typedConfigurations()->filename(el::Level::Info);
+    filename.erase(0, destDir.length());
+    std::string symlinkFile = program + ".INFO";
+    std::string oldSymLink = destDir + symlinkFile;
+    remove(oldSymLink.c_str());
+    if(symlinkat(filename.c_str(), fd, symlinkFile.c_str()) != 0)
+      LOG(ERROR) << ".INFO symlink set fail!";
+
+    filename = l->typedConfigurations()->filename(el::Level::Warning);
+    filename.erase(0, destDir.length());
+    symlinkFile = program + ".WARN";
+    oldSymLink = destDir + symlinkFile;
+    remove(oldSymLink.c_str());
+    if(symlinkat(filename.c_str(), fd, symlinkFile.c_str()) != 0)
+      LOG(ERROR) << ".WARN symlink set fail!";
+
+    filename = l->typedConfigurations()->filename(el::Level::Error);
+    filename.erase(0, destDir.length());
+    symlinkFile = program + ".ERROR";
+    oldSymLink = destDir + symlinkFile;
+    remove(oldSymLink.c_str());
+    if(symlinkat(filename.c_str(), fd, symlinkFile.c_str()) != 0)
+      LOG(ERROR) << ".ERROR symlink set fail!";
+
+    filename = l->typedConfigurations()->filename(el::Level::Fatal);
+    filename.erase(0, destDir.length());
+    symlinkFile = program + ".FATAL";
+    oldSymLink = destDir + symlinkFile;
+    remove(oldSymLink.c_str());
+    if(symlinkat(filename.c_str(), fd, symlinkFile.c_str()) != 0)
+      LOG(ERROR) << ".FATAL symlink set fail!";
 
     return 0;
   }
