b5f4a584c6b9f3dbbd727b74ba7f2cfbbf38bbec
mwan <mwan@cedar.com>
2016-07-14 09:03:20 +0800

    Mannual Order revised

--- ManualOrder/include/ManualOrder.h
+++ ManualOrder/include/ManualOrder.h
@@ -4,15 +4,25 @@
 #include "CPlusPlusCode/ProtoBufMsg.pb.h"
 #include "ProtoBufMsgHub.h"
 #include "CedarHelper.h"
+using namespace std;
+
 
 class ManualOrder {
 
 public:
   ManualOrder();
   void run();
-
+  void autorun();
 private:
   ProtoBufMsgHub msgHub;
+
+  void test1(); 
+  void test2();
+  void sendSingleOrder(string id, string code, TradeDirection buy_sell, int quantity); 
+  void cancelSingleOrder(string id, string ref_id);
+  
+  void updataTest();
+
 
   int queryAction();
   int queryEnterOrder();
@@ -24,13 +34,19 @@ private:
   std::string queryRefID();
   std::string queryCode();
   std::string queryChan();
+  std::string queryCancelID();
   //ReturnType queryRtnType();
   RequestType queryOrdType();
   PositionDirection queryOrdPosition();
   //OrderAction queryOrdAction();
   int onMsg(MessageBase);
+  int onUpdateMsg(MessageBase);
+  int onMsgTest(MessageBase);
 
+  void updateTestBoardcast();
+  
   std::string sendAddr;
+  std::string pubAddr;
 };
 
 #endif
--- ManualOrder/src/ManualOrder.cpp
+++ ManualOrder/src/ManualOrder.cpp
@@ -1,17 +1,87 @@
 #include <iostream>
 #include "ManualOrder.h"
+#include <unistd.h>
+#include "BatTest.h"
 
 ManualOrder::ManualOrder() {
   ProtoBufHelper::setupProtoBufMsgHub(msgHub);
-  msgHub.registerCallback(std::bind(&ManualOrder::onMsg, this,
+  msgHub.registerCallback(std::bind(&ManualOrder::onUpdateMsg, this,
         std::placeholders::_1));
 
   CedarJsonConfig::getInstance().getStringByPath("SendToAddress", sendAddr);
+  CedarJsonConfig::getInstance().getStringByPath("PublisherAddress", pubAddr);
 }
 
 int ManualOrder::onMsg(MessageBase msg) {
-  LOG(INFO) << "MessageBase:" << msg.type();
+  MsgType type = msg.type();
+  ResponseMessage rmsg = ProtoBufHelper::unwrapMsg<ResponseMessage>(msg);
+
+  std::cout << "type:" << rmsg.type() << std::endl;
+
+
+  std::cout << "id:" << rmsg.id() << std::endl;
+  std::cout << "error code:" << rmsg.error_code() << std::endl;
+  LOG(INFO) << "id:" << rmsg.id();
 }
+
+
+int ManualOrder::onMsgTest(MessageBase msg) {
+  MsgType type = msg.type();
+  ResponseMessage rmsg = ProtoBufHelper::unwrapMsg<ResponseMessage>(msg);
+
+  LOG(INFO) << "id" << rmsg.id();
+}
+
+int ManualOrder::onUpdateMsg(MessageBase msg) {
+  std::cout<< "*" << std::endl;
+  MsgType type= msg.type();
+  MarketUpdate mUpdate = ProtoBufHelper::unwrapMsg<MarketUpdate>(msg);
+  std::cout << "code:" <<mUpdate.code() << std::endl;
+  std::cout << "exchange:" <<mUpdate.exchange() << std::endl;
+  std::cout << "pre_close_price:" <<mUpdate.pre_close_price() << std::endl;
+  std::cout << "open_price:" <<mUpdate.open_price() << std::endl;
+  std::cout << "last price:" <<mUpdate.last_price() << std::endl;
+  std::cout << "recv time:" <<mUpdate.recv_timestamp() << std::endl;
+ 
+  std::cout << "bid_price 1:" <<mUpdate.bid_price(0) << std::endl; 
+  std::cout << "bid_price 2:" <<mUpdate.bid_price(1) << std::endl;
+  std::cout << "bid_price 3:" <<mUpdate.bid_price(2) << std::endl;
+  std::cout << "bid_price 4:" <<mUpdate.bid_price(3) << std::endl;
+  std::cout << "bid_price 5:" <<mUpdate.bid_price(4) << std::endl;
+
+
+  std::cout << "bid_volume 1:" <<mUpdate.bid_volume(0) << std::endl; 
+  std::cout << "bid_volume 2:" <<mUpdate.bid_volume(1) << std::endl;
+  std::cout << "bid_volume 3:" <<mUpdate.bid_volume(2) << std::endl;
+  std::cout << "bid_volume 4:" <<mUpdate.bid_volume(3) << std::endl;
+  std::cout << "bid_volume 5:" <<mUpdate.bid_volume(4) << std::endl;
+
+
+  std::cout << "ask_price 1:" <<mUpdate.ask_price(0) << std::endl; 
+  std::cout << "ask_price 2:" <<mUpdate.ask_price(1) << std::endl;
+  std::cout << "ask_price 3:" <<mUpdate.ask_price(2) << std::endl;
+  std::cout << "ask_price 4:" <<mUpdate.ask_price(3) << std::endl;
+  std::cout << "ask_price 5:" <<mUpdate.ask_price(4) << std::endl;
+
+  std::cout << "ask_volume 1:" <<mUpdate.ask_volume(0) << std::endl; 
+  std::cout << "ask_volume 2:" <<mUpdate.ask_volume(1) << std::endl;
+  std::cout << "ask_volume 3:" <<mUpdate.ask_volume(2) << std::endl;
+  std::cout << "ask_volume 4:" <<mUpdate.ask_volume(3) << std::endl;
+  std::cout << "ask_volume 5:" <<mUpdate.ask_volume(4) << std::endl;
+
+  std::cout << std::endl;
+
+  LOG(INFO) << "[REALTIMETEST]id:" << mUpdate.code() << "|ask1 price:" << mUpdate.ask_price(0);
+  LOG(INFO) << "[REALTIMETEST]id:" << mUpdate.code()  << "|ask1 volume:" << mUpdate.ask_volume(0);
+  
+  LOG(INFO) << "[REALTIMETEST]id:" << mUpdate.code()  << "|bid1 price:" << mUpdate.bid_price(0);
+  LOG(INFO) << "[REALTIMETEST]id:" << mUpdate.code()  << "|bid1 volume:" << mUpdate.bid_volume(0);
+
+  LOG(INFO) << "[REALTIMETEST]id:" << mUpdate.code()  << "|last price:" << mUpdate.last_price();
+
+}
+
+
 
 void ManualOrder::run() {
   while (true) {
@@ -24,6 +94,139 @@ void ManualOrder::run() {
       break;
   }
 }
+
+void ManualOrder::sendSingleOrder(string id, string code, TradeDirection buy_sell, int quantity) {
+  OrderRequest order;
+  order.set_response_address(CedarHelper::getResponseAddr());
+
+  order.set_id(id);
+  order.set_code(code);
+  order.set_buy_sell(buy_sell);
+  order.set_trade_quantity(quantity);
+  order.set_limit_price(2.9);
+  order.set_open_close(PositionDirection::OPEN_POSITION);
+  order.set_exchange(SHSE);
+
+  msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_ORDER_REQUEST, order));
+}
+
+
+void ManualOrder::cancelSingleOrder(string id, string ref_id) {
+  OrderRequest order;
+  order.set_response_address(CedarHelper::getResponseAddr());
+  order.set_type(TYPE_CANCEL_ORDER_REQUEST);
+  order.set_id(id);
+  order.set_cancel_order_id(ref_id);
+
+
+  msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_ORDER_REQUEST, order));
+}
+
+void ManualOrder::test1() {
+  int count = 1;
+  int i = 0;
+  unsigned int mic=1;
+  static std::string respAddr = CedarHelper::getResponseAddr();
+  string tmp;
+
+  for (i=0;i<20;i++) {  
+    tmp = to_string(i);
+    sendSingleOrder(tmp,"518880",TradeDirection::LONG_BUY,100);
+  //  sleep(1);
+  }
+
+  for (i=0;i<20;i++) {  
+    tmp = to_string(i);
+  //  sleep(1);
+    cancelSingleOrder("4897",tmp); 
+  }
+  
+  
+  getchar();
+}
+
+
+
+void ManualOrder::test2() {
+  int count = 1;
+  int i = 0;
+  unsigned int mic=1;
+  static std::string respAddr = CedarHelper::getResponseAddr();
+  string tmp;
+
+  for (i=0;i<50;i++) {  
+    tmp = to_string(i);
+    sendSingleOrder(tmp,"518880",TradeDirection::LONG_BUY,100);
+  //  sleep(1);
+    cancelSingleOrder("4897",tmp); 
+  }
+  getchar();
+}
+
+void ManualOrder::updataTest() {
+  DataRequest dataReq;
+  dataReq.set_code("000404");
+  dataReq.set_exchange(SZSE);
+
+  msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_DATAREQUEST,dataReq));
+
+  
+  dataReq.set_code("000418");
+  dataReq.set_exchange(SZSE);
+
+  msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_DATAREQUEST,dataReq));
+  
+  dataReq.set_code("002340");
+  dataReq.set_exchange(SZSE);
+
+  msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_DATAREQUEST,dataReq));
+  
+  dataReq.set_code("000060");
+  dataReq.set_exchange(SZSE);
+
+ // msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_DATAREQUEST,dataReq));
+  
+  getchar();
+}
+
+
+void ManualOrder::updateTestBoardcast() {
+  DataRequest dataReq;
+  dataReq.set_code("000404");
+  dataReq.set_exchange(SZSE);
+
+  msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_DATAREQUEST,dataReq));
+
+  msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_DATAREQUEST,dataReq));
+  dataReq.set_code("000418");
+  dataReq.set_exchange(SZSE);
+  msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_DATAREQUEST,dataReq));
+  
+  dataReq.set_code("002340");
+  dataReq.set_exchange(SZSE);
+  msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_DATAREQUEST,dataReq));
+  
+  
+  dataReq.set_code("000060");
+  dataReq.set_exchange(SZSE);
+  msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_DATAREQUEST,dataReq));
+  
+
+  msgHub.addSubscription(pubAddr, "000061.SZSE");
+  msgHub.addSubscription(pubAddr, "002340.SZSE");
+  msgHub.addSubscription(pubAddr, "000418.SZSE");
+  msgHub.addSubscription(pubAddr, "000404.SZSE");
+  std::cout<<"subscripe complete";
+
+  getchar();
+}
+
+void ManualOrder::autorun() {
+  //test1();
+  updateTestBoardcast();
+}
+
+
 
 //OrderAction ManualOrder::queryOrdAction(){
 //  char value;
@@ -86,8 +289,34 @@ std::string ManualOrder::queryChan(){
 //  }
 //}
 
+
+std::string ManualOrder::queryCancelID() {
+  std::string ID;
+  std::cout << "CancelID:" << std::endl;
+  std::cin >> ID;
+  return ID;
+}
+
+
 int ManualOrder::queryCancelOrder() {
-  printf("queryCancel order\n");
+  std::cout << "\nNewOrder Request\n";
+  static std::string respAddr = CedarHelper::getResponseAddr();
+
+  OrderRequest order;
+  order.set_response_address(respAddr);
+
+  order.set_id(queryID());
+  order.set_code(queryCode());
+  order.set_buy_sell(querySide());
+  order.set_trade_quantity(queryOrderQty());
+  order.set_type(queryOrdType());
+  order.set_limit_price(queryPrice());
+  order.set_open_close(queryOrdPosition());
+  order.set_cancel_order_id(queryCancelID());
+
+  msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_ORDER_REQUEST, order));
+
+  return 0;
 }
 
 int ManualOrder::queryAction() {
@@ -160,14 +389,16 @@ double ManualOrder::queryPrice() {
 RequestType ManualOrder::queryOrdType() {
   char value;
   std::cout << std::endl
-  << "1) Market" << std::endl
-  << "2) Limit" << std::endl
+  << "1) Limit" << std::endl
+  << "2) Market" << std::endl
+  << "3) Cancel" << std::endl
   << "OrdType: ";
 
   std::cin >> value;
   switch (value) {
     case '1': return RequestType::TYPE_LIMIT_ORDER_REQUEST;
     case '2': return RequestType::TYPE_MARKET_ORDER_REQUEST;
+    case '3': return RequestType::TYPE_CANCEL_ORDER_REQUEST;
     default: throw std::exception();
   }
 }
@@ -186,7 +417,7 @@ int ManualOrder::queryEnterOrder() {
   order.set_type(queryOrdType());
   order.set_limit_price(queryPrice());
   order.set_open_close(queryOrdPosition());
-
+  order.set_exchange(SZSE);
   msgHub.pushMsg(sendAddr, ProtoBufHelper::wrapMsg(TYPE_ORDER_REQUEST, order));
 
   return 0;
