bfd6ee69a6ffa5700e401bacc8fc2a3bf121f42f
infra <infra@cedarcapital.cn>
2016-08-09 15:34:35 +0800

    add StockService for crontab and logStash conf

---
+++ ShareConfig/StockService.py
@@ -0,0 +1,33 @@
+import subprocess
+import sys
+import getopt
+import os
+
+def start():
+  subprocess.Popen(['/home/infra/logstash-2.3.3/bin/logstash', '-f', '/home/infra/logstash-2.3.3/configure/receive.conf'])
+  subprocess.Popen(['python', '/home/infra/hydra/LogMonitor/MonitorServer.py'])
+
+def stop():
+  subprocess.call('/home/infra/logstash-2.3.3/stop.sh')
+  subprocess.call('/home/infra/hydra/LogMonitor/stop.sh')
+
+def service(argv):
+  try:
+    opts, args = getopt.getopt(argv,"se",["start","stop"])
+  except getopt.GetoptError:
+    print "parameter not recognized!"
+    sys.exit(2)
+
+  for opt, arg in opts:
+    if opt in ("-s", "--start"):
+      start()
+    elif opt in ("-e", "--stop"):
+      stop()
+    else:
+      print "no valid option"
+
+if __name__ == "__main__":
+  if (len(sys.argv) < 2):
+    print "parameter not recognized!"
+    sys.exit(0)
+  service(sys.argv[1:])
